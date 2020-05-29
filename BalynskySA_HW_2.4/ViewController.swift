4fa76160692f13e69e8a074b4a5fef5eaa052a6a
SBalynsky <bsa89@mail.ru>
2020-05-29 09:13:49 +0300

    Initial Commit

---
+++ BalynskySA_HW_2.4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BalynskySA_HW_2.4
+//
+//  Created by Macbook on 29.05.2020.
+//  Copyright © 2020 Balynsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
